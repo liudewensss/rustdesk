9a74f0a721497a1ef45422009c8bfe1f264de2db
liudewensss <1442154146@qq.com>
2025-11-11 23:34:30 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -1046,7 +1046,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://yc.zczy.cc:21114".to_owned()
 }
 
 #[inline]
